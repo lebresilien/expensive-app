b8352f92aba89f3c6b76b13e37f5d814f56b9180
lebresilien <petitotapondjou@gmail.com>
2024-11-16 14:58:14 +0100

    perform dashboard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,80 @@
-import { Image, StyleSheet, Platform, SafeAreaView, StatusBar, ScrollView } from 'react-native';
+import { Image, StyleSheet, Platform, SafeAreaView, StatusBar, ScrollView, Button } from 'react-native';
 
 import { ThemedView } from '@/components/ThemedView';
 import { useThemeColor } from '@/hooks/useThemeColor';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemeIcon } from '@/components/ThemeIcon';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import api from '../lib/api';
 
 type ThemedTextProps = {
   lightColor?: string;
   darkColor?: string;
 };
 
+export type Transaction = {
+  id: string;
+  name: string;
+  amount: number,
+  date: string
+};
+
+const ItemList = ({
+  name, 
+  date, 
+  amount,
+  isExpens,
+  background,
+  backgroundColor, 
+  color
+  }: {
+    name: string,
+    date: string,
+    amount: number,
+    isExpens: boolean,
+    background: string,
+    backgroundColor: string, 
+    color: string
+  } ) => (
+    <ThemedView style={[styles.itemContainer, { backgroundColor: backgroundColor }]}>
+      <ThemedView style={styles.itemFirstBlock}>
+        <ThemedView style={[{ backgroundColor: background }, styles.avatar]}>
+          <ThemedText style={[{textTransform: 'uppercase'}, {color}]}>{ name.charAt(0) }</ThemedText>
+        </ThemedView>
+        <ThemedView>
+          <ThemedText type='link' style={styles.bold}>{ name }</ThemedText>
+          <ThemedText type='link' style={{fontWeight: '100'}}>{ date }</ThemedText>
+        </ThemedView>
+      </ThemedView>
+      <ThemedText style={{color: isExpens ? 'red' : 'green'}}>{ amount } FCFA</ThemedText>
+    </ThemedView>
+  )
+
 export default function HomeScreen({ lightColor, darkColor}: ThemedTextProps) {
 
   const [selected, setSelected] = useState<'incomes' | 'expenses'>('incomes');
+  const [expenses, setExpenses] = useState<Transaction[] | []>([]);
+  const [incomes, setIncomes] = useState<Transaction[] | []>([]);
+  const [totalIcomes, setTotalIncomes] = useState(0);
+  const [totalExpenses, setTotalExpenses] = useState(0);
 
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'contentBackground');
   const background = useThemeColor({ light: lightColor, dark: darkColor }, 'inactiveTint');
-  
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'icon');
+
+  useEffect(() => {
+    api.get('transactions')
+    .then((res) => {
+      console.log(res.data.data)
+      if(res.data.success) {
+        setIncomes(res.data.data.incomes);
+        setExpenses(res.data.data.expenses);
+        setTotalExpenses(parseFloat(res.data.data.totalExpenses));
+        setTotalIncomes(parseFloat(res.data.data.totalIncomes));
+      }
+    })
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
 
@@ -53,7 +110,7 @@ export default function HomeScreen({ lightColor, darkColor}: ThemedTextProps) {
 
             <ThemedText type='defaultSemiBold'>Solde</ThemedText>
 
-            <ThemedText type='defaultSemiBold' style={styles.bold}>26000 FCFA</ThemedText>
+            <ThemedText type='defaultSemiBold' style={styles.bold}>{ totalIcomes - totalExpenses } FCFA</ThemedText>
 
               <ThemedView style={styles.content}>
 
@@ -69,7 +126,7 @@ export default function HomeScreen({ lightColor, darkColor}: ThemedTextProps) {
 
                   </ThemedView>
 
-                  <ThemedText type='link'>5000 fcfa</ThemedText>
+                  <ThemedText type='link'>{ totalIcomes } FCFA</ThemedText>
 
                 </ThemedView>
 
@@ -85,7 +142,7 @@ export default function HomeScreen({ lightColor, darkColor}: ThemedTextProps) {
 
                   </ThemedView>
 
-                  <ThemedText type='link'>5000 fcfa</ThemedText>
+                  <ThemedText type='link'>{ totalExpenses } FCFA</ThemedText>
 
                 </ThemedView>
 
@@ -97,18 +154,51 @@ export default function HomeScreen({ lightColor, darkColor}: ThemedTextProps) {
                 
             <ThemedView
               onTouchStart={() => setSelected('incomes')}
-              style={[styles.swiperItem, selected === 'incomes' && styles.selected]}
+              style={[styles.swiperItem, selected === 'incomes' && ([styles.selected, { borderBottomColor: background }])]}
             >
-              <ThemedText style={[selected === 'incomes' && {color: 'red'}]}>revenus</ThemedText>
+              <ThemedText style={[selected === 'incomes' && { color: background, fontWeight: 'bold' }]}>revenus</ThemedText>
             </ThemedView>
 
             <ThemedView 
               onTouchStart={() => setSelected('expenses')}
-              style={[styles.swiperItem, selected === 'expenses' && styles.selected]}
+              style={[styles.swiperItem, selected === 'expenses' && ([styles.selected, { borderBottomColor: background }])]}
             >
-              <ThemedText style={[selected === 'expenses' && {color: 'red'}]}>depenses</ThemedText>
+              <ThemedText style={[selected === 'expenses' && { color: background, fontWeight: 'bold' }]}>depenses</ThemedText>
             </ThemedView>
 
+          </ThemedView>
+
+          <ThemedView style={{marginHorizontal: 10, rowGap: 10}}>
+            {selected === "expenses" ? 
+              <>
+                {expenses.map((item, index) => (
+                  <ItemList
+                    key={index}
+                    name={item.name}
+                    amount={item.amount}
+                    date={item.date}
+                    background={background}
+                    backgroundColor={backgroundColor}
+                    isExpens={true}
+                    color={color}
+                  />
+                ))}
+              </>:
+              <>
+                {incomes.map((item, index) => (
+                  <ItemList
+                    key={index}
+                    name={item.name}
+                    amount={item.amount}
+                    date={item.date}
+                    background={background}
+                    backgroundColor={backgroundColor}
+                    isExpens={false}
+                    color={color}
+                  />
+                ))}
+              </>
+            }
           </ThemedView>
 
         </ThemedView>
@@ -150,6 +240,14 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 5
   },
+  avatar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 2
+  },
   card: {
     borderRadius: 10,
     rowGap: 10,
@@ -184,8 +282,7 @@ const styles = StyleSheet.create({
     paddingBottom: 5
   },
   selected: {
-    borderBottomWidth: 2,
-    borderBottomColor: 'red',
+    borderBottomWidth: 2
   },
   stepContainer: {
     gap: 8,
@@ -194,4 +291,21 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: 'bold'
   },
+  itemContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    rowGap: 3,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  itemFirstBlock: {
+    display: 'flex',
+    flexDirection: 'row',
+    columnGap: 15,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
